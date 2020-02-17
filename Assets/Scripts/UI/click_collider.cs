cf2844243dede6e39aafc7b8aaac5daf9c8bb72e
Markin45 <50928999+Markin45@users.noreply.github.com>
2020-02-18 00:40:19 +0300

    Нажатие колайдера

---
+++ Assets/Scripts/UI/click_collider.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class click_collider : MonoBehaviour
+{
+
+
+    private void OnMouseUp()
+    {
+        Debug.Log("Клик по Колайдеру");
+    }
+
+
+}
